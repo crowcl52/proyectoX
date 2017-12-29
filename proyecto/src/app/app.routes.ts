1115403879d2e525634b7245e9fa6285afd76eaf
eduardo_sanchez_R <eduardosanchez@aori.mx>
2017-12-29 15:46:41 -0600

    origins

--- proyecto/src/app/app.routes.ts
+++ proyecto/src/app/app.routes.ts
@@ -2,11 +2,11 @@ import {RouterModule, Routes} from '@angular/router';
 
 import {HomeComponent} from "./components/home/home.component";
 import {ContactComponent} from "./components/contact/contact.component";
-// import {CatalogoComponent} from "./components/catalogo/catalogo.component";
+import {OriginsComponent} from "./components/origins/origins.component";
 
 const app_routes: Routes = [
     {path:'inicio',component:HomeComponent},
-    // {path:'catalogo',component:CatalogoComponent},
+    {path:'origenes',component:OriginsComponent},
     {path:'contacto',component:ContactComponent},
     {path:'**', pathMatch:'full', redirectTo:'inicio'}
 ];
